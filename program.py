98317b1563d607ec9892efbccd4526f7a7f1b3cd
Rayr19 <rubenalonsoyr@gmail.com>
2026-04-14 11:16:56 -0500

    V2: Implementada estructura secuencial de costos

--- program.py
+++ program.py
@@ -2,3 +2,10 @@ print("-------------------------------------------------------")
 print("++++++++++++++ 3D SERVICES S.A. ++++++++++++++")
 print("+++++ SISTEMA DE CÁLCULO DE MATERIALES +++++")
 print("-------------------------------------------------------")
+
+volumen = float(input("Ingrese el volumen de la pieza (ml): "))
+costo_ml = float(input("Ingrese el costo de la resina por ml ($): "))
+
+costo_total = volumen * costo_ml
+
+print (f"\nEl costo total de manufactura es: ${costo_total:.2f}")
